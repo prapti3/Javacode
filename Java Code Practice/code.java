e12d202a801a44784542b3039bf82547378ad49d
prapti3 <prapti.chavan3@gmail.com>
2025-09-04 11:27:48 +0530

    Java code file added

---
+++ Java Code Practice/code.java
@@ -0,0 +1,28 @@
+public class code{
+    public static void main(String[] args){
+        
+
+        int a = 2;
+        boolean bool = true;
+        char ch = 'A';
+        double db = 3.14;
+
+
+        System.out.println("Integer : "+ a);
+        System.out.println("double : "+db);
+        System.out.println("Boolean : "+ bool);
+        System.out.println("Character : "+ch);
+        
+
+
+
+
+        
+
+
+
+
+
+
+    }
+}
