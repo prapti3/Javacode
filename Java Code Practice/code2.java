be797770d3f971003156f02688d316de4737f0ca
prapti3 <prapti.chavan3@gmail.com>
2025-09-04 11:47:58 +0530

    add files

---
+++ Java Code Practice/code2.java
@@ -0,0 +1,38 @@
+public class code2 {
+public static void main(String[] args) {
+    /**
+     * - Use +, -, *, /, % operators
+       - Demonstrate assignment operators (=, +=, -=, *=, /=)
+       - Show the results of each operation
+     */
+
+    int a = 10;
+    int b = 11;
+
+    System.out.println("Addition : "+ (a+b));
+    System.out.println("Subtraction : "+ (a-b));
+    System.out.println("Multiplication : "+ (a*b));
+    System.out.println("Division : "+ (a/b));
+    System.out.println("Modulus : "+ (a%b));
+
+    a += 5; // a = a + 5
+    System.out.println("After a += 5, a = " + a);
+    a -= 3; // a = a - 3
+    System.out.println("After a -= 3, a = " + a);
+    a *= 2; // a = a * 2
+    System.out.println("After a *= 2, a = " + a);
+    a /= 4; // a = a / 4
+    System.out.println("After a /= 4, a = " + a);
+    a %= 3; // a = a % 3
+    System.out.println("After a %= 3, a = " + a);
+
+
+
+
+    
+
+
+
+
+}
+}
