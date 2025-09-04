9d338d395c8830ca9a44a6baeab897ab404ee9f7
prapti3 <prapti.chavan3@gmail.com>
2025-09-04 11:53:47 +0530

    add files code3

---
+++ Java Code Practice/code3.java
@@ -0,0 +1,34 @@
+public class code3 {
+    public static void main(String[] args) {
+        /**
+         * - Use comparison operators (==, !=, <, >, <=, >=)
+           - Use logical operators (&&, ||, !)
+           - Print boolean results with explanatory messages
+         */
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println("x == y: " + (x == y)); // false
+        System.out.println("x != y: " + (x != y)); // true
+        System.out.println("x < y: " + (x < y));   // true
+        System.out.println("x > y: " + (x > y));   // false
+        System.out.println("x <= y: " + (x <= y)); // true
+        System.out.println("x >= y: " + (x >= y)); // false
+
+        System.out.println(" ");
+
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b: " + (a && b)); // false
+        System.out.println("a || b: " + (a || b)); // true
+        System.out.println("!a: " + (!a));           // false
+        System.out.println("!b: " + (!b));           // true
+
+
+        
+
+
+
+    }
+}
