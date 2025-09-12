1a395e1ab7af94c3ef9d4f074d01dd37bcaded96
prapti3 <prapti.chavan3@gmail.com>
2025-09-12 23:53:33 +0530

    doc file

--- Java Code Practice/code4.java
+++ Java Code Practice/code4.java
@@ -7,39 +7,29 @@ public class code4 {
 
     public static void main(String[] args) {
         int i = 5;
-
-        System.out.println("Initial value of i: " + i);
+        System.out.println("Original :"+ i);
 
         // Using pre-increment
         int preIncrement = ++i; // i is incremented first, then assigned
-        System.out.println("After pre-increment (++i): " + preIncrement);
-        System.out.println("Value of i after pre-increment: " + i);
 
-        // Resetting i for clarity
-        i = 5;
-        System.out.println("Reset value of i: " + i);
+        System.out.println("pre-increment : " + preIncrement);
+   
 
         // Using post-increment
         int postIncrement = i++; // i is assigned first, then incremented
-        System.out.println("After post-increment (i++): " + postIncrement);
-        System.out.println("Value of i after post-increment: " + i);
+        System.out.println("Post-increment : " + postIncrement);
 
-        // Resetting i for clarity
-        i = 5;
-        System.out.println("Reset value of i: " + i);
 
         // Using pre-decrement
-        int preDecrement = --i; // i is decremented first, then assigned
-        System.out.println("After pre-decrement (--i): " + preDecrement);
-        System.out.println("Value of i after pre-decrement: " + i);
+        int j =6;
 
-        // Resetting i for clarity
-        i = 5;
-        System.out.println("Reset value of i: " + i);
+        int preDecrement = --j; // i is decremented first, then assigned
+        System.out.println("pre-decrement : " + preDecrement);
+     
 
         // Using post-decrement
-        int postDecrement = i--; // i is assigned first, then decremented
-        System.out.println("After post-decrement (i--): " + postDecrement);
-        System.out.println("Value of i after post-decrement: " + i);
+        int postDecrement = j--; // i is assigned first, then decremented
+        System.out.println("post-decrement : " + postDecrement);
+       
 }
 }
---
+++ Java Code Practice/code5.java
@@ -0,0 +1,13 @@
+public class code5 {
+    public static void main(String[] args) {
+        /*
+         * 
+         * - Show operator precedence with complex expressions
+        * - Demonstrate implicit and explicit type casting
+        * - Print results with explanations
+         * 
+         * 
+         * 
+        */
+    }
+}
