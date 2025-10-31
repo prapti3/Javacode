195184640fa28edc2912f57f100368bd4edc87d0
prapti3 <prapti.chavan3@gmail.com>
2025-10-31 19:09:18 +0530

    JAVA

--- src/Java_DS/Java_Data_structure/ArrayList/sort_ArrayList.java
+++ src/Java_DS/Java_Data_structure/ArrayList/sort_ArrayList.java
@@ -3,6 +3,7 @@ package Java_DS.Java_Data_structure.ArrayList;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 public class sort_ArrayList {
     public static void main(String[] args){
@@ -15,6 +16,10 @@ public class sort_ArrayList {
         for(String i : cars){
             System.out.println(i);
         }
+
+        var bikes = new ArrayList<String>();
+        List<String> MotorBike = new ArrayList<>();
+
 
 
     }
---
+++ src/Java_DS/Java_Data_structure/LinkedList/Array_LinkedList.java
@@ -0,0 +1,15 @@
+package Java_DS.Java_Data_structure.LinkedList;
+
+import java.util.LinkedList;
+
+public class Array_LinkedList {
+    public static void main(String[] args){
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+
+    }
+}
